75ff5ce2663d923bb1bb682897d9678bc6497d37
Olli Aalto <olli.aalto@mystes.fi>
2017-05-28 11:57:35 +0300

    First test for listed item

--- src/Model.elm
+++ src/Model.elm
@@ -1,12 +1,21 @@
-module Model exposing (Model, init)
+module Model exposing (Model, Page(..), init)
 
 import Message exposing (Msg)
+import ShoppingList.Model as SList exposing (Model, init)
+
+
+type Page
+    = ShoppingListPage
 
 
 type alias Model =
-    {}
+    { currentPage : Page
+    , shoppingList : SList.Model
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model, Cmd.none )
+    ( Model ShoppingListPage SList.init
+    , Cmd.none
+    )
--- src/View.elm
+++ src/View.elm
@@ -2,11 +2,13 @@ module View exposing (view)
 
 import Html exposing (Html, text, div, img)
 import Html.Attributes exposing (src)
-import Model exposing (Model)
+import Model exposing (Model, Page(..))
 import Message exposing (Msg)
+import ShoppingList.View as SList exposing (view)
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        []
+    case model.currentPage of
+        ShoppingListPage ->
+            SList.view model.shoppingList
